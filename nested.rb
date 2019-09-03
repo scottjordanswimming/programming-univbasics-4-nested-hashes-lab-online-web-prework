876c2c5bfa26c7d0f47b8958766cc128ecd5a66f
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-03 20:44:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -134,6 +134,10 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+dennis_new_info = "Assembly"
+     programmer_hash[:dennis_ritchie][:languages] = dennis_new_info
+     
+     return programmer_hash
+     
 
 end
